e1397af302db707f3e923abe6875204f9d6016ff
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2017-03-25 16:28:21 +0100

    Add project files.

---
+++ Futura.SignalR/LoggingHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using NLog.SignalR;
+
+namespace SignalR.Net45
+{
+    public class LoggingHub : Hub<ILoggingHub>
+    {
+        public void Log(LogEvent logEvent) {
+            Clients.Others.Log(logEvent);
+        }
+
+
+    }
+}
---
+++ Futura.SignalR/Scripts/app.js
@@ -0,0 +1,42 @@
+﻿var app = angular.module("app", ["ngAnimate", "ngTouch", "ui.grid"]);
+
+app.controller("MainCtrl",
+[
+    "$scope", "$http", "uiGridConstants", function($scope, $http, uiGridConstants) {
+
+        $scope.myData = [];
+        $scope.gridOptions = {
+            enableFiltering: true,
+            data: [],
+            columnDefs: [
+                { field: "TimeStamp", displayName: "Date", width: 225, enableFiltering: false },
+                {
+                    field: "Level",
+                    displayName: "Level",
+                    width: 100,
+                    filter: {
+                        //term: "Info",
+                        noTerm: true,
+                        type: uiGridConstants.filter.SELECT,
+                        selectOptions: [
+                            { value: "Fatal", label: "Fatal" },
+                            { value: "Error", label: "Errors" },
+                            { value: "Warn", label: "Warnings" },
+                            { value: "Info", label: "Info" },
+                            { value: "Debug", label: "Debug" },
+                            { value: "Trace", label: "Trace" }
+                        ]
+                    }
+                },
+                { field: "Message", cellClass: "Message", minWidth: 500, maxWidth: 1000, enableFiltering: false }
+            ],
+            sortInfo: { fields: ["TimeStamp"], directions: ["desc"] },
+            afterSelectionChange: function(rowItem, event) { alert(rowItem.entity.Message); }
+        };
+
+        $scope.changeData = function(logEvent) {
+            $scope.gridOptions.data.push(logEvent);
+        };
+
+    }
+]);
